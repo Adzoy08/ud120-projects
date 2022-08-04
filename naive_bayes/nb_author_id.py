c19d6ad4a62592efe4a37583c3071d8c0267823b
Samuel Boadi <adzoy08@yahoo.com>
2022-08-04 21:49:20 +0000

    chore: Import GaussianNB

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -24,7 +24,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 ##############################################################
 # Enter Your Code Here
-
+from sklearn.naive_bayes import GaussianNB
 
 
 ##############################################################
