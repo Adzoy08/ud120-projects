f95e848dba5a5690a6d0ac38e6daf1d59f4024d7
Samuel Boadi <adzoy08@yahoo.com>
2022-08-08 21:04:22 +0000

    chore: Import DecisionTree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,6 +24,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
 
 
 #########################################################
